54903591e5397d82195ca0136a477b9922dc2fe0
atiwat1113 <atiwat.deepo@gmail.com>
2021-11-02 16:08:23 +0700

    add readme

--- database.js
+++ database.js
@@ -1,9 +1,12 @@
 const mysql = require('mysql2');
 require('dotenv').config()
 
-module.exports = mysql.createConnection({
+const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
 });
+
+
+module.exports = db;
