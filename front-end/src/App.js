8f0ac2b7f9bdded78a7386736a9585f5bc22a24a
Kittipong <kittipong.deevee@gmail.com>
2021-11-07 18:18:52 +0700

    Add all card (except the one we haven't discussed_
    Add 4 cards, comments, and TODOs for the next tasks.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,80 +1,230 @@
+// Import things.
 import {
   TextField,
   makeStyles,
   Button,
   Paper,
-  Grid,
   Divider,
+  Box,
 } from "@material-ui/core";
-
+import { Alert } from "@mui/material";
+import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import { useState } from "react";
 
 const useStyles = makeStyles({
+  // Handle all TextField style.
   field: {
     marginTop: 10,
     marginBottom: 15,
     display: "block",
-    
   },
 });
 
-
 function App() {
   const classes = useStyles();
+
+  // useState with variables.
   const [cardNum, setCardNum] = useState("");
   const [holderName, setHolderName] = useState("");
   const [expireDate, setExpireDate] = useState("");
+  const [locationID, setLocationID] = useState("");
+  const [newName, setNewName] = useState("");
+  const [latitude, setLatitude] = useState("");
+  const [longitude, setLongitude] = useState("");
+  const [addressName, setAddressName] = useState("");
 
-  const handleSubmit = (e) => {
+  // useState with an actual state.
+  const [deleteConfirmation, setDeleteConfirmation] = useState(false);
+
+  //TODO: INSERT credit card.
+  const insertCreditCard = (e) => {
     e.preventDefault();
+  };
 
-    if (cardNum && holderName && expireDate) {
-      console.log(cardNum, holderName, expireDate);
-    }
+  //TODO: UPDATE saved address.
+  const updateSavedAddress = (e) => {
+    e.preventDefault();
+  };
+
+  //TODO: INSERT new adress.
+  const insertNewAddress = (e) => {
+    e.preventDefault();
+  };
+
+  //TODO: DELETE account.
+  const deleteAccount = (e) => {
+    setDeleteConfirmation((prev) => !prev);
+    console.log(deleteConfirmation);
+    e.preventDefault();
   };
 
   return (
-    <Grid container spacing={2}>
-      <Grid item xs={4}>
+    // Column box arrangement. Each row contains a "row" box with a form (paper) inside.
+    <Box sx={{ display: "flex", flexDirection: "column" }}>
+      <Box sx={{ display: "flex", alignItems: "flex-start" }}>
         <Paper
           style={{
             margin: 16,
-            padding: 16
+            padding: 32,
           }}
         >
           <h3>INSERT CREDIT CARD</h3>
-          <Divider style={{ background: 'black', marginTop: 16}} variant="fullWidth" />
-          <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+          <Divider
+            style={{ background: "black", marginTop: 16 }}
+            variant="fullWidth"
+          />
+          <form noValidate autoComplete="off" onSubmit={insertCreditCard}>
             <TextField
-              key="Confirmation Code"
               onChange={(e) => setCardNum(e.target.value)}
               className={classes.field}
               label="Credit Card Number"
               required
+              fullWidth
             />
             <TextField
               onChange={(e) => setHolderName(e.target.value)}
               className={classes.field}
               label="Card Holder Name"
-              required 
+              required
+              fullWidth
             />
             <TextField
               onChange={(e) => setExpireDate(e.target.value)}
               className={classes.field}
               label="Expire Date"
               required
+              fullWidth
             />
 
-              <Button type="submit" variant="outlined" color="primary">
-                Submit
-              </Button>
-
+            <Button
+              type="submit"
+              variant="outlined"
+              color="primary"
+              fullWidth
+              style={{ marginTop: 10 }}
+            >
+              Submit
+            </Button>
           </form>
         </Paper>
-      </Grid>
-    </Grid>
+      </Box>
+      <Box sx={{ display: "flex", alignItems: "flex-start" }}>
+        <Paper
+          style={{
+            margin: 16,
+            padding: 32,
+          }}
+        >
+          <h3>UPDATE SAVED ADDRESS</h3>
+          <Divider
+            style={{ background: "black", marginTop: 16 }}
+            variant="fullWidth"
+          />
+          <form noValidate autoComplete="off" onSubmit={updateSavedAddress}>
+            <TextField
+              onChange={(e) => setLocationID(e.target.value)}
+              className={classes.field}
+              label="Location ID"
+              required
+              fullWidth
+            />
+            <TextField
+              onChange={(e) => setNewName(e.target.value)}
+              className={classes.field}
+              label="New name to update"
+              required
+              fullWidth
+            />
+
+            <Button
+              type="submit"
+              variant="outlined"
+              color="primary"
+              fullWidth
+              style={{ marginTop: 10 }}
+            >
+              Submit
+            </Button>
+          </form>
+        </Paper>
+        <Paper
+          style={{
+            margin: 16,
+            padding: 32,
+          }}
+        >
+          <h3>INSERT NEW ADDRESS (NOSQL)</h3>
+          <Divider
+            style={{ background: "black", marginTop: 16 }}
+            variant="fullWidth"
+          />
+          <form noValidate autoComplete="off" onSubmit={insertNewAddress}>
+            <TextField
+              onChange={(e) => setLatitude(e.target.value)}
+              className={classes.field}
+              label="Latitude"
+              required
+              fullWidth
+            />
+            <TextField
+              onChange={(e) => setLongitude(e.target.value)}
+              className={classes.field}
+              label="Longitude"
+              required
+              fullWidth
+            />
+            <TextField
+              onChange={(e) => setAddressName(e.target.value)}
+              className={classes.field}
+              label="Address Name"
+              required
+              fullWidth
+            />
+
+            <Button
+              type="submit"
+              variant="outlined"
+              color="primary"
+              fullWidth
+              style={{ marginTop: 10 }}
+            >
+              Submit
+            </Button>
+          </form>
+        </Paper>
+      </Box>
+      <Box sx={{ display: "flex", alignItems: "flex-start" }}>
+        <Paper
+          style={{
+            margin: 16,
+            padding: 32,
+          }}
+        >
+          <h3>DELETE ACCOUNT</h3>
+          <Divider
+            style={{ background: "black", marginTop: 16 }}
+            variant="fullWidth"
+          />
+          <form noValidate autoComplete="off" onSubmit={deleteAccount}>
+            <Button
+              type="submit"
+              variant="outlined"
+              color="primary"
+              fullWidth
+              style={{ marginTop: 16 }}
+            >
+              Submit
+            </Button>
+          </form>
+        </Paper>
+        {deleteConfirmation ? (
+          <Alert severity="warning" style={{marginTop: 16}}>
+            TODO: Implement Alert Dailog
+          </Alert>
+        ) : null}
+      </Box>
+    </Box>
   );
 }
 
