8a4b398677b44223d5804e1cdeafa58cc12d234a
Kittipong <kittipong.deevee@gmail.com>
2021-11-12 01:46:09 +0700

    Complete a mock method to DeleteAccount.
    Complete a mock method to DeleteAccount.

--- front-end/src/pages/DeleteAccount.js
+++ front-end/src/pages/DeleteAccount.js
@@ -1,66 +1,73 @@
 // Import things.
 import {
-    makeStyles,
-    Button,
-    Paper,
-    Divider,
-    Box,
-    Typography,
-    createTheme,
-    ThemeProvider,
-  } from "@material-ui/core";
-  import { Alert } from "@mui/material";
-  import { useState } from "react";
-  import "bootstrap/dist/css/bootstrap.min.css";
-  import "../styles/App.css";
-  import Navbar from "../components/Navbar";
-  
-  const useStyles = makeStyles((theme) => ({
-    // Handle all TextField style.
-    field: {
-      marginTop: 10,
-      marginBottom: 15,
-      display: "block",
-    },
-    toolbar: theme.mixins.toolbar,
-  }));
-  
-  const theme = createTheme({
-    typography: {
-      fontFamily: ["Manrope"].join(","),
-    },
-  });
-  
-  //TODO: Implement HTTP Method
-  const deleteAccount = () => {
-  
+  makeStyles,
+  Button,
+  Paper,
+  Divider,
+  Box,
+  Typography,
+  createTheme,
+  ThemeProvider,
+} from "@material-ui/core";
+import { Alert } from "@mui/material";
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/App.css";
+import Navbar from "../components/Navbar";
+import axios from "axios";
 
-  };
-  
-  function DeleteAccount() {
-    const classes = useStyles();
-  
-    // useState.
-    const [deleteConfirmation, setDeleteConfirmation] = useState(false);
-  
-    return (
-      <ThemeProvider theme={theme}>
-        <Box sx={{ display: "flex", flexDirection: "column" }}>
-          <Navbar></Navbar>
-  
-          <div className={classes.toolbar}></div>
-  
-          <Box sx={{ display: "flex", alignItems: "flex-start" }}>
-            <Paper
-              style={{
-                margin: 16,
-                padding: 32,
-              }}
-            >
-              <Typography variant="h5">
-                DELETE ACCOUNT
-              </Typography>
-              <Divider
+const useStyles = makeStyles((theme) => ({
+  // Handle all TextField style.
+  field: {
+    marginTop: 10,
+    marginBottom: 15,
+    display: "block",
+  },
+  toolbar: theme.mixins.toolbar,
+}));
+
+const theme = createTheme({
+  typography: {
+    fontFamily: ["Manrope"].join(","),
+  },
+});
+
+//TODO: Implement HTTP Method
+async function deleteAccount(e) {
+  e.preventDefault();
+
+  try {
+    const return_status = await axios
+      .delete("/customers")
+      .then((res) => res.status);
+    console.log(return_status);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+function DeleteAccount() {
+  const classes = useStyles();
+
+  // useState.
+  const [deleteConfirmation, setDeleteConfirmation] = useState(false);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Box sx={{ display: "flex", flexDirection: "column" }}>
+        <Navbar></Navbar>
+
+        <div className={classes.toolbar}></div>
+
+        <Box sx={{ display: "flex", alignItems: "flex-start" }}>
+          <Paper
+            style={{
+              margin: 16,
+              padding: 32,
+            }}
+          >
+            <Typography variant="h5">DELETE ACCOUNT</Typography>
+            <Divider
               style={{ background: "black", marginTop: 16 }}
               variant="fullWidth"
             />
@@ -75,17 +82,16 @@ import {
                 Delete
               </Button>
             </form>
-            </Paper>
-            {deleteConfirmation ? (
+          </Paper>
+          {deleteConfirmation ? (
             <Alert severity="warning" style={{ marginTop: 16 }}>
               TODO: Implement Alert Dailog
             </Alert>
           ) : null}
-          </Box>
         </Box>
-      </ThemeProvider>
-    );
-  }
-  
-  export default DeleteAccount;
-  
+      </Box>
+    </ThemeProvider>
+  );
+}
+
+export default DeleteAccount;
