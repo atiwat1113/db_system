0134d0192cb320d74a993207ddf2d618bdb0ba38
Kittipong <kittipong.deevee@gmail.com>
2021-11-20 16:14:17 +0700

    Construct a table to show query for QUERY Riders
    Construct a table to show query for QUERY Riders

--- front-end/src/pages/QueryRiders.js
+++ front-end/src/pages/QueryRiders.js
@@ -9,6 +9,12 @@ import {
   Typography,
   createTheme,
   ThemeProvider,
+  Table,
+  TableContainer,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
 } from "@material-ui/core";
 import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -37,6 +43,19 @@ function QueryRiders() {
 
   // useState with variables.
   const [stationName, setStationName] = useState("");
+  const [returnData, setReturnData] = useState([]);
+
+  function createData(name, calories, fat, carbs, protein) {
+    return { name, calories, fat, carbs, protein };
+  }
+
+  const rows = [
+    createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+    createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+    createData("Eclair", 262, 16.0, 24, 6.0),
+    createData("Cupcake", 305, 3.7, 67, 4.3),
+    createData("Gingerbread", 356, 16.0, 49, 3.9),
+  ];
 
   //TODO: Implement HTTP Method
   async function queryRiders(e) {
@@ -50,8 +69,32 @@ function QueryRiders() {
               stationName: stationName,
             },
           })
-          .then((res) => res.data);
-        console.log(return_data);
+          .then((res) => res.data[0]);
+
+        //TODO: Alter some return data.
+        return_data.map((row) => {
+          for (const [key, value] of Object.entries(row)) {
+            if (key === "is_available"){
+              if (value === 1){
+                row[key] = 'Available';
+              } else {
+                row[key] = 'Unavailable'
+              }
+            }
+
+            if (key === "rating"){
+              if (!value){
+                row[key] = 'TBD'
+              }
+            }
+          }
+        })
+
+        //TODO: Set ReturnData to Table
+        setReturnData(return_data);
+
+        
+
       } catch (err) {
         console.log(err);
       }
@@ -70,6 +113,7 @@ function QueryRiders() {
             style={{
               margin: 16,
               padding: 32,
+              minWidth: 400,
             }}
           >
             <Typography variant="h5">QUERY RIDERS FROM STATION</Typography>
@@ -96,6 +140,46 @@ function QueryRiders() {
               </Button>
             </form>
           </Paper>
+
+          <TableContainer
+            component={Paper}
+            style={{
+              margin: 16,
+              minWidth: 400,
+            }}
+          >
+            <Table
+              sx={{ minWidth: 650 }}
+              size="small"
+              aria-label="a dense table"
+            >
+              <TableHead>
+                <TableRow>
+                  <TableCell>First Name</TableCell>
+                  <TableCell>Last Name</TableCell>
+                  <TableCell>Ref Number</TableCell>
+                  <TableCell>Is Available</TableCell>
+                  <TableCell>Rating</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {returnData.map((row) => (
+                  <TableRow
+                    key={row.name}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell key={0} component="th" scope="row">
+                      {row.first_name}
+                    </TableCell>
+                    <TableCell>{row.last_name}</TableCell>
+                    <TableCell>{row.ref_no}</TableCell>
+                    <TableCell>{row.is_available}</TableCell>
+                    <TableCell>{row.rating}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
         </Box>
       </Box>
     </ThemeProvider>
