10ceabc7aaf20001dfd6513cc9d65220ec5ec83e
Tarm <tk.kalavan>
2021-11-21 00:15:01 +0700

    add: mongo router

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const testRouter = require('./routes/test');
+const mongoRouter = require('./routes/mongo');
 const cors = require('cors');
 require('dotenv').config()
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB);
 
 const app = express();
 
@@ -15,8 +15,15 @@ app.use('*', cors({
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/test', testRouter);
+// app.use('/test', testRouter);
+app.use('/mongo', mongoRouter);
 
-app.listen(3000, () => {
-    console.log('Listening to port 3000');
+
+// connect to mongo before listen
+mongoose.connect(process.env.MONGODB, {useNewUrlParser: true, useUnifiedTopology: true })
+.then( result => {
+    console.log("Connected to mongoose");
+    app.listen(3000, () => {
+        console.log('Listening to port 3000');
+    })
 })
---
+++ routes/mongo.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const Location = require('../mongo/Location');
+const Customer = require('../mongo/Customer');
+
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+    console.log('req:', req.body)
+    const user_id = "UID00007"; // hard-coded
+    const { lat, long, sub_district, district, province, postal_code, name } = req.body;
+    const latitude = parseFloat(lat);
+    const longitude = parseFloat(long);
+    
+    const location = await Location.findOne({ latitude: latitude, longitude: longitude }).exec();
+    console.log('location:',location)
+    const newAddr = { 
+        name: name, 
+        location_id: location? location._id:null 
+    };
+
+    if(location){ // location already exists
+        //* CASE 1: the user already added this location -> update user's saved_address name
+        
+        if(location.saved_by.some(e => e.user_id === user_id)){
+            console.log('case 1')
+            Customer.find({ user_id: user_id })
+            .then(user => {
+                    user.forEach(addr => {
+                        if(addr.location_id == location._id){
+                            addr.name = name;
+                        }
+                    })
+            })
+            .catch(err => {
+                    console.log('case 1 error:', err);
+                    res.status(500).end();
+            })
+            res.status(200).end()
+        }
+        //* CASE 2: the user haven't added this location -> update saved_by & add to saved_address
+        else{
+            console.log('case 2');
+            try{
+                await Location.updateOne({ latitude: latitude, longitude: longitude }, { $push: { saved_by: {user_id: user_id} } });
+                await Customer.updateOne({ user_id: user_id }, { $push: { saved_address: newAddr } });
+                res.status(200).end();
+            }
+            catch (err) {
+                console.log('case 2 error:', err);
+                res.status(500).end();
+            }
+        }
+    }
+    //* CASE 3: location not exist -> create one
+    else{
+        console.log('case 3')
+        const newLocation = new Location({
+            latitude,
+            longitude,
+            sub_district,
+            district,
+            province,
+            postal_code,
+            saved_by: [{ user_id: user_id }]
+        });
+        try{
+            await newLocation.save();
+            await Customer.updateOne({ user_id: user_id }, { $push: { saved_address: newAddr } });
+            res.status(200).end();
+        }
+        catch (err){
+            console.log('case 3 error:', err);
+            res.status(500).end();
+        }
+    }
+    console.log('==============')
+})
+module.exports = router;
